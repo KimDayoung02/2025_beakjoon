520fa22b054767ba139e66daed6136ecdfcb10c3
KimDayoung <73173270+KimDayoung02@users.noreply.github.com>
2025-01-17 17:34:24 +0900

    [Bronze V] Title: A-B, Time: 112 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,21 @@
+const readline=require("readline");
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+let input='';
+
+
+rl.on("line", (line)=>{
+    input=line;
+    rl.close();
+});
+
+rl.on("close", ()=>{
+
+    let a=parseInt(input[0]);
+    let b=parseInt(input[2]);
+    console.log(a-b);
+    process.exit();
+});
